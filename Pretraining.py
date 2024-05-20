ce7bdb3bb8fac62f499ad9c359e1051291dc59f7
root <rasheibani@gmail.com>
2024-05-20 10:02:29 +1000

    adding more call backs

---
+++ Pretraining.py
@@ -0,0 +1,29 @@
+# 0-0.25
+Pretraining = ["A_Average-Regular_Approach2",
+               "ZOZ_perspire_Approach2",
+               "HI_Average-Regular_Approach2",
+               "ZSM_perspire_Approach2",
+               "AEJ_Average-Regular_Approach2",
+
+               # 0.25 - 0.5
+               "JCD_Average-Regular_Approach1",
+               "KIW_arial_Approach1",
+               "MMK_arial_Approach1",
+               "WW_Average-Regular_Approach1",
+               "BTM_Average-Regular_Approach2",
+
+               # .5 - 0.75
+               'OGB_perspire_Approach2',
+               'AEY_Average-Regular_Approach1',
+               'AKV_Average-Regular_Approach1',
+               'KIW_Average-Regular_Approach1',
+               'NPP_Average-Regular_Approach1',
+               'NWW_Average-Regular_Approach1',
+
+               # 0.75-1
+               'CBS_Average-Regular_Approach1',
+               'DAQ_Average-Regular_Approach1',
+               'DMF_Average-Regular_Approach1',
+               'EHX_Average-Regular_Approach1',
+               'VNM_Average-Regular_Approach1',
+               'ZGC_Average-Regular_Approach1']
